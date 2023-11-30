0981e636ecb37771df0dae63b22bad19411cf236
CA01972006 <CA01972006@st.kawahara.ac.jp>
2023-11-30 11:44:52 +0900

    最終的な初期化。今後、このリポジトリに触れる時は新しい要素を追加する時だけ

--- Game/Game/Game.cpp
+++ Game/Game/Game.cpp
@@ -1,14 +1,11 @@
 #include "stdafx.h"
 #include "Game.h"
-#include "GameCode/クレアエクション/ClairAction.h"
 
 #include "Operation/Operation.h"
 #include "DimensionalEffect/DimensionalEffect.h"
 #include "DimensionalElement/DimensionalElement.h"
 Game::Game()
 {
-	S_GameCode.P_Clair = NewGO<ClairAction>(0, "clair");
-
 	S_Operation.P_Mouse = NewGO<Mouse>(1, "mouse");
 	S_Operation.P_Controller = NewGO<Controller>(1, "controller");
 
@@ -21,8 +18,6 @@ Game::Game()
 }
 Game::~Game()
 {
-	DeleteGO(S_GameCode.P_Clair);
-
 	DeleteGO(S_Operation.P_Mouse);
 	DeleteGO(S_Operation.P_Controller);
 
