9933fd735f5a0df4b8533dec786b5a960381c3d5
CA01972006 <CA01972006@st.kawahara.ac.jp>
2024-02-29 13:58:33 +0900

    GameCodeクラスの初期化

--- Game/GameCode/Actor/GameActor.h
+++ Game/GameCode/Actor/GameActor.h
@@ -1,11 +1,7 @@
 #pragma once
-#include "Code/Player.h"
-
-class Player;
-
 enum class ActorClass
 {
-	PLAYER
+	
 };
 
 class GameActor : public IGameObject
@@ -13,42 +9,26 @@ class GameActor : public IGameObject
 public:
 	void ActorNewGO()
 	{
-		P_Player =
-		NewGO<Player>
-		(0, "player");
+		
 	}
 	void ActorNewGO(ActorClass Class)
 	{
-		switch (Class)
-		{
-		case ActorClass::PLAYER:
-			P_Player =
-			NewGO<Player>
-			(0, "player");
-			break;
-		}
+		
 	}
 
 	void ActorFindGO()
 	{
-		P_Player =
-		FindGO<Player>
-		("player");
+		
 	}
 
 	void ActorDeleteGO()
 	{
-		DeleteGO(P_Player);
+		
 	}
 	void ActorDeleteGO(ActorClass Class)
 	{
-		switch (Class)
-		{
-		case ActorClass::PLAYER:
-			DeleteGO(P_Player);
-			break;
-		}
+		
 	}
 public:
-	Player* P_Player = nullptr;
+	
 };
--- Game/GameCode/Other/GameOther.h
+++ Game/GameCode/Other/GameOther.h
@@ -1,11 +1,7 @@
 #pragma once
-#include "Code/GameCamera.h"
-
-class GameCamera;
-
 enum class OtherClass
 {
-	GAMECAMERA
+	
 };
 
 class GameOther : public IGameObject
@@ -13,42 +9,26 @@ class GameOther : public IGameObject
 public:
 	void OtherNewGO()
 	{
-		P_GameCamera =
-		NewGO<GameCamera>
-		(0, "gamecamera");
+		
 	}
 	void OtherNewGO(OtherClass Class)
 	{
-		switch (Class)
-		{
-		case OtherClass::GAMECAMERA:
-			P_GameCamera =
-			NewGO<GameCamera>
-			(0, "gamecamera");
-			break;
-		}
+		
 	}
 
 	void OtherFindGO()
 	{
-		P_GameCamera =
-		FindGO<GameCamera>
-		("gamecamera");
+		
 	}
 
 	void OtherDeleteGO()
 	{
-		DeleteGO(P_GameCamera);
+		
 	}
 	void OtherDeleteGO(OtherClass Class)
 	{
-		switch (Class)
-		{
-		case OtherClass::GAMECAMERA:
-			DeleteGO(P_GameCamera);
-			break;
-		}
+		
 	}
 public:
-	GameCamera* P_GameCamera = nullptr;
+	
 };
